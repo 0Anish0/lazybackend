3a843d6de4cb52ed1adffc1da0415bfffa1c1e6d
Parwinderkaur385 <parwinderkaur385@gmail.com>
2024-11-29 11:28:46 +0530

    cors add localhost

--- server.js
+++ server.js
@@ -2,31 +2,33 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./utils/db');
 const authRoutes = require('./apiRoutes');
-const cors = require('cors');
 const multer = require('multer');  // Ensure multer is imported
 const upload = multer({ storage: multer.memoryStorage() }).single('live_image');  // Memory storage
 
 const app = express();
-app.use(express.json());  // This is for parsing JSON bodies
-app.use(express.urlencoded({ extended: true }));  // This is for parsing URL-encoded data, especially for form-data
+app.use(express.json());  // For parsing JSON bodies
+app.use(express.urlencoded({ extended: true }));  // For parsing URL-encoded data, especially for form-data
 
 // Connect to MongoDB
 connectDB();
 
-// Enable CORS
-const corsOptions = {
-    origin: 'https://lazycrazyy.netlify.app', // Your frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    credentials: true, // Include credentials if needed (e.g., cookies)
-};
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+const allowedOrigins = ['https://lazycrazyy.netlify.app', 'http://localhost:3000'];
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); // If you use cookies or authorization headers
+    next();
+});
 
 // Routes
 // Apply multer upload as middleware for your API that handles file upload
 app.post('/api/signup', upload, authRoutes);  // Use multer middleware here
-app.use('/api', authRoutes);  // Make sure your other routes are defined after multer
+app.use('/api', authRoutes);  // Define other routes after multer
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
